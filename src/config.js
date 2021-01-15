bce5995194b6eb789fa50e6ceb3d949bb37d2f8d
David Feldt <dsfeldt@gmail.com>
2021-01-15 10:50:23 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_51I6U1yIDJpMLtBsSd6ZsW1SeWNzz6R7y6QQG8kthYw8Qig2CV97BvUiSqlymdVGn8v0z3koYzaFe7UgNN1ctyyrM00gmJ2ayJE",
+
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://su2xvgoi8d.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_bnx3Xle3f",
+      APP_CLIENT_ID: "14v3sdojs47ibt67c0b7n5l0tj",
+      IDENTITY_POOL_ID: "us-east-2:656541d1-cc3d-4aed-83f3-3b74935405a5"
+    }
+  };
